2aae2eb422b23b62f49520a51026be3259dc195c
Ori Cohen <orioriori2000@gmail.com>
2024-05-03 13:14:31 +0300

    resolution changed 160x120

--- main2.cpp
+++ main2.cpp
@@ -2,9 +2,10 @@
 #include <iostream>
 #include <chrono>
 #include <gpiod.hpp>
+#include <vector>
 
-#define CameraWIDTH  640
-#define CameraHeight 480
+#define CameraWIDTH 160  // 400 //640
+#define CameraHeight 120 // 240 //480
 
 using namespace cv;
 using namespace std;
@@ -17,11 +18,13 @@ int LeftLanePos, RightLanePos, frameCenter, laneCenter, Result, laneEnd;
 
 vector<int> histrogramLane;
 vector<int> histrogramLaneEnd;
-Point2f Source[] = {Point2f(145,250), Point2f(430,250), Point2f(55,CameraHeight), Point2f(535, CameraHeight)}; //do not change
-Point2f Destination[] = {Point2f(160, 230), Point2f(410, 230), Point2f(160, 480), Point2f(410, 480)};
+// Point2f Source[] = {Point2f(145, 250), Point2f(430, 250), Point2f(55, CameraHeight), Point2f(535, CameraHeight)}; // do not change
+// Point2f Destination[] = {Point2f(160, 230), Point2f(410, 230), Point2f(160, 480), Point2f(410, 480)};
 // Point2f Source[] = {Point2f(50,145),Point2f(255,145),Point2f(15,195), Point2f(290,195)};
 // Point2f Destination[] = {Point2f(75,0),Point2f(220,0),Point2f(75,240), Point2f(220,240)};
-
+// take the source and destination points and changed them accordingly for 160x120
+Point2f Source[] = {Point2f(20, 60), Point2f(115, 60), Point2f(0, 120), Point2f(145, 120)};
+Point2f Destination[] = {Point2f(20, 0), Point2f(115, 0), Point2f(20, 120), Point2f(115, 120)};
 
 VideoCapture cap(0);
 
@@ -29,13 +32,13 @@ stringstream ss;
 
 void setup()
 {
-    cap.set(CAP_PROP_FRAME_WIDTH, CameraWIDTH);  // Adjust based on your webcam specifications
+    cap.set(CAP_PROP_FRAME_WIDTH, CameraWIDTH);   // Adjust based on your webcam specifications
     cap.set(CAP_PROP_FRAME_HEIGHT, CameraHeight); // Adjust based on your webcam specifications
-    cap.set(CAP_PROP_FPS,0);          
-    cap.set(CAP_PROP_BRIGHTNESS, 0);
-    cap.set(CAP_PROP_CONTRAST, 80);
-    cap.set(CAP_PROP_SATURATION, 50);
-    cap.set(CAP_PROP_GAIN, 10);
+    cap.set(CAP_PROP_FPS, 100);
+    cap.set(CAP_PROP_BRIGHTNESS, 10);
+    cap.set(CAP_PROP_CONTRAST, 0);   // 50
+    cap.set(CAP_PROP_SATURATION, 0); // 50
+    cap.set(CAP_PROP_GAIN, 20);      // 10
 }
 
 void captureFrames()
@@ -52,12 +55,12 @@ void Perspective()
     line(frame, Source[1], Source[3], Scalar(0, 0, 255), 2);
     line(frame, Source[3], Source[2], Scalar(0, 0, 255), 2);
     line(frame, Source[2], Source[0], Scalar(0, 0, 255), 2);
-    line(frame, Destination[0], Destination[1], Scalar(0, 255,0), 2);
+    line(frame, Destination[0], Destination[1], Scalar(0, 255, 0), 2);
     line(frame, Destination[1], Destination[3], Scalar(0, 255, 0), 2);
     line(frame, Destination[3], Destination[2], Scalar(0, 255, 0), 2);
     line(frame, Destination[2], Destination[0], Scalar(0, 255, 0), 2);
     Matrix = getPerspectiveTransform(Source, Destination);
-    warpPerspective(frame, framePers, Matrix, Size(CameraWIDTH, CameraHeight));
+    warpPerspective(frame, framePers, Matrix, Size(CameraWIDTH, CameraHeight)); // 120, 120
 }
 
 void Threshold()
@@ -67,8 +70,8 @@ void Threshold()
     Canny(frameGray, frameEdge, 900, 900, 3, false);
     add(frameThresh, frameEdge, frameFinal);
     cvtColor(frameFinal, frameFinal, COLOR_GRAY2RGB);
-    cvtColor(frameFinal, frameFinalDuplicate, COLOR_RGB2BGR); // used in histrogram function only
-    cvtColor(frameFinal, frameFinalDuplicate1, COLOR_RGB2BGR);   //used in histrogram function only
+    cvtColor(frameFinal, frameFinalDuplicate, COLOR_RGB2BGR);  // used in histrogram function only
+    cvtColor(frameFinal, frameFinalDuplicate1, COLOR_RGB2BGR); // used in histrogram function only
 }
 
 void Histrogram()
@@ -78,7 +81,9 @@ void Histrogram()
 
     for (int i = 0; i < CameraWIDTH; i++) // frame.size().width = 400
     {
-        ROILane = frameFinalDuplicate(Rect(i, 280, 1, 200)); 
+        // ROILane = frameFinalDuplicate(Rect(i, 140, 1, 100));
+        // take the ROILane and adjust the values from 400x240 to 160x120
+        ROILane = frameFinalDuplicate(Rect(i, 60, 1, 60));
         divide(255, ROILane, ROILane);
         histrogramLane.push_back((int)(sum(ROILane)[0]));
         ROILaneEnd = frameFinalDuplicate1(Rect(i, 0, 1, CameraHeight));
@@ -92,24 +97,35 @@ void Histrogram()
 void LaneFinder()
 {
     vector<int>::iterator LeftPtr;
-    LeftPtr = max_element(histrogramLane.begin(), histrogramLane.begin() + 270);
+    // LeftPtr = max_element(histrogramLane.begin(), histrogramLane.begin() + 150);
+    LeftPtr = max_element(histrogramLane.begin(), histrogramLane.begin() + 60);
     LeftLanePos = distance(histrogramLane.begin(), LeftPtr);
 
     vector<int>::iterator RightPtr;
-    RightPtr = max_element(histrogramLane.begin() + 370, histrogramLane.end());
+    // RightPtr = max_element(histrogramLane.begin() +250, histrogramLane.end());
+    RightPtr = max_element(histrogramLane.begin() + 100, histrogramLane.end());
     RightLanePos = distance(histrogramLane.begin(), RightPtr);
 
-    line(frameFinal, Point2f(LeftLanePos, 0), Point2f(LeftLanePos, 480), Scalar(0, 255, 0), 2);
-    line(frameFinal, Point2f(RightLanePos, 0), Point2f(RightLanePos, 480), Scalar(0, 255, 0), 2);
+    // line(frameFinal, Point2f(LeftLanePos, 0), Point2f(LeftLanePos, 240), Scalar(0, 255, 0), 2);
+    // line(frameFinal, Point2f(RightLanePos, 0), Point2f(RightLanePos, 240), Scalar(0, 255, 0), 2);
+    // adjust the values from 400x240 to 160x120
+    line(frameFinal, Point2f(LeftLanePos, 0), Point2f(LeftLanePos, 120), Scalar(0, 255, 0), 2);
+    line(frameFinal, Point2f(RightLanePos, 0), Point2f(RightLanePos, 120), Scalar(0, 255, 0), 2);
 }
 
 void LaneCenter()
 {
     laneCenter = (RightLanePos - LeftLanePos) / 2 + LeftLanePos;
-    frameCenter = 285;
 
-    line(frameFinal, Point2f(laneCenter, 0), Point2f(laneCenter, 480), Scalar(0, 255, 0), 3);
-    line(frameFinal, Point2f(frameCenter, 0), Point2f(frameCenter, 480), Scalar(255, 0, 0), 3);
+    // frameCenter = 188;
+    // adjust the values from 400x240 to 160x120
+    frameCenter = 75;
+
+    // line(frameFinal, Point2f(laneCenter, 0), Point2f(laneCenter, 240), Scalar(0, 255, 0), 3);
+    // line(frameFinal, Point2f(frameCenter, 0), Point2f(frameCenter, 240), Scalar(255, 0, 0), 3);
+    // adjust the values from 400x240 to 160x120
+    line(frameFinal, Point2f(laneCenter, 0), Point2f(laneCenter, 120), Scalar(0, 255, 0), 3);
+    line(frameFinal, Point2f(frameCenter, 0), Point2f(frameCenter, 120), Scalar(255, 0, 0), 3);
 
     Result = laneCenter - frameCenter;
 }
@@ -128,119 +144,160 @@ int main()
         cerr << "Error opening webcam." << endl;
         return -1;
     }
-
     cout << "Webcam is opened" << endl;
-
     while (1)
     {
-        auto start = chrono::system_clock::now();
+        int64 start = getTickCount();
         captureFrames();
+        if (frame.empty())
+        {
+            cerr << "Error capturing the frame." << endl;
+            break;
+        }
         Perspective();
         Threshold();
         Histrogram();
         LaneFinder();
         LaneCenter();
- 
+
         if (laneEnd > 3000) // stop
         {
+            ss.str(" ");
+            ss.clear();
+            ss << " Lane End";
+            putText(frame, ss.str(), Point2f(5, 15), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(0, 0, 255), 1);
             lines.set_values({0, 1, 1, 1});
             cout << "Lane End" << endl;
         }
         if (Result == 0)
         {
+            ss.str(" ");
+            ss.clear();
+            ss << "Result = " << Result << " Move Forward";
+            // putText(frame, ss.str(), Point2f(1, 50), 0, 1, Scalar(0, 0, 255), 2);
+            putText(frame, ss.str(), Point2f(5, 15), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(0, 0, 255), 1);
             lines.set_values({0, 0, 0, 0});
             cout << "Forward" << endl;
         }
 
         else if (Result > 0 && Result < 10)
         {
+            ss.str(" ");
+            ss.clear();
+            ss << "Result = " << Result << "bMove Right";
+            putText(frame, ss.str(), Point2f(5, 15), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(0, 0, 255), 1);
             lines.set_values({0, 0, 0, 1}); // decimal = 1
             cout << "Right1" << endl;
         }
 
         else if (Result >= 10 && Result < 20)
         {
+            ss.str(" ");
+            ss.clear();
+            ss << "Result = " << Result << "bMove Right";
+            putText(frame, ss.str(), Point2f(5, 15), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(0, 0, 255), 1);
             lines.set_values({0, 0, 1, 0}); // decimal = 2
             cout << "Right2" << endl;
         }
 
         else if (Result >= 20)
         {
+            ss.str(" ");
+            ss.clear();
+            ss << "Result = " << Result << "bMove Right";
+            putText(frame, ss.str(), Point2f(5, 15), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(0, 0, 255), 1);
             lines.set_values({0, 0, 1, 1}); // decimal = 3
             cout << "Right3" << endl;
         }
 
         else if (Result < 0 && Result > -10)
         {
+            ss.str(" ");
+            ss.clear();
+            ss << "Result = " << Result << " Move Left";
+            putText(frame, ss.str(), Point2f(5, 15), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(0, 0, 255), 1);
             lines.set_values({0, 1, 0, 0}); // decimal =4
             cout << "Left1" << endl;
         }
 
         else if (Result <= -10 && Result > -20)
         {
+            ss.str(" ");
+            ss.clear();
+            ss << "Result = " << Result << " Move Left";
+            putText(frame, ss.str(), Point2f(5, 15), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(0, 0, 255), 1);
             lines.set_values({0, 1, 0, 1}); // decimal = 5
             cout << "Left2" << endl;
         }
 
         else if (Result <= -20)
         {
-            lines.set_values({0, 1, 1, 0}); // decimal = 6
-            cout << "Left3" << endl;
-        }
-        if (laneEnd > 3000)
-        {
-            ss.str(" ");
-            ss.clear();
-            ss << " Lane End";
-            putText(frame, ss.str(), Point2f(1, 50), 0, 1, Scalar(255, 0, 0), 2);
-        }
-
-        else if (Result == 0)
-        {
-            ss.str(" ");
-            ss.clear();
-            ss << "Result = " << Result << " Move Forward";
-            putText(frame, ss.str(), Point2f(1, 50), 0, 1, Scalar(0, 0, 255), 2);
-        }
-
-        else if (Result > 0)
-        {
-            ss.str(" ");
-            ss.clear();
-            ss << "Result = " << Result << "bMove Right";
-            putText(frame, ss.str(), Point2f(1, 50), 0, 1, Scalar(0, 0, 255), 2);
-        }
-
-        else if (Result < 0)
-        {
             ss.str(" ");
             ss.clear();
             ss << "Result = " << Result << " Move Left";
-            putText(frame, ss.str(), Point2f(1, 50), 0, 1, Scalar(0, 0, 255), 2);
+            putText(frame, ss.str(), Point2f(5, 15), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(0, 0, 255), 1);
+            lines.set_values({0, 1, 1, 0}); // decimal = 6
+            cout << "Left3" << endl;
         }
+        // if (laneEnd > 3000)
+        // {
+        // }
+
+        // else if (Result == 0)
+        // {
+        //     ss.str(" ");
+        //     ss.clear();
+        //     ss << "Result = " << Result << " Move Forward";
+        //     // putText(frame, ss.str(), Point2f(1, 50), 0, 1, Scalar(0, 0, 255), 2);
+        //     putText(frame, ss.str(), Point2f(10, 30), FONT_HERSHEY_COMPLEX_SMALL, 0.5, Scalar(0, 0, 255), 2);
+        // }
+
+        // else if (Result > 0)
+        // {
+        //     ss.str(" ");
+        //     ss.clear();
+        //     ss << "Result = " << Result << "bMove Right";
+        //     putText(frame, ss.str(), Point2f(10, 30), FONT_HERSHEY_COMPLEX_SMALL, 0.5, Scalar(0, 0, 255), 2);
+        // }
+
+        // else if (Result < 0)
+        // {
+        //     ss.str(" ");
+        //     ss.clear();
+        //     ss << "Result = " << Result << " Move Left";
+        //     putText(frame, ss.str(), Point2f(10, 30), FONT_HERSHEY_COMPLEX_SMALL, 0.5, Scalar(0, 0, 255), 2);
+        // }
 
         namedWindow("orignal", WINDOW_KEEPRATIO);
         moveWindow("orignal", 0, 100);
         resizeWindow("orignal", 640, 480);
         imshow("orignal", frame);
-
         namedWindow("Perspective", WINDOW_KEEPRATIO);
         moveWindow("Perspective", 640, 100);
         resizeWindow("Perspective", 640, 480);
         imshow("Perspective", framePers);
-
         namedWindow("Final", WINDOW_KEEPRATIO);
         moveWindow("Final", 1280, 100);
         resizeWindow("Final", 640, 480);
         imshow("Final", frameFinal);
-
-        auto key = waitKey(1);
-        auto end = chrono::system_clock::now();
-        chrono::duration<double> elapsed_seconds = end - start;
-
-        float t = elapsed_seconds.count();
-        int FPS = 1 / t;
-        cout << "FPS = " << FPS << endl;
+        waitKey(1);
+        // if (waitKey(5) >= 0){
+        //     break;
+        // }
+        // char c = (char)waitKey(1);
+        // while (c == 'q' || c == 'Q' || c == 27)
+        // {
+        //     ss.str(" ");
+        //     ss.clear();
+        //     ss << " Lane End";
+        //     putText(frame, ss.str(), Point2f(5, 15), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(0, 0, 255), 1);
+        //     lines.set_values({0, 1, 1, 1});
+        //     cout << "Lane End" << endl;
+        //     if ((char)waitKey(1) == 'c')
+        //         break;
+        // }
+        double fps = getTickFrequency() / (getTickCount() - start);
+        cout << "FPS : " << fps << endl;
     }
 
     return 0;
